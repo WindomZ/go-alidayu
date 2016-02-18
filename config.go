fe1dec807f6212c57ba7fba021142960e529e732
WindomZ <git.windomz@gmail.com>
2016-02-18 10:54:57 +0800

    style: change file name

--- config.go
+++ config.go
@@ -3,6 +3,11 @@ package alidayu
 import ()
 
 const (
+	URL_PRODUCTION string = "https://eco.taobao.com/router/rest"      // 正式环境 (或者: http://gw.api.taobao.com/router/rest)
+	URL_SANDBOX    string = "http://gw.api.tbsandbox.com/router/rest" // 沙箱环境 (或者: http://gw.api.tbsandbox.com/router/rest)
+)
+
+const (
 	DEFAULT_COURIER_SIZE  int = 3
 	DEFAULT_CAPACITY_SIZE int = 10
 )
--- urls.go
+++
@@ -1,8 +0,0 @@
-package alidayu
-
-import ()
-
-const (
-	URL_PRODUCTION string = "https://eco.taobao.com/router/rest"      // 正式环境 (或者: http://gw.api.taobao.com/router/rest)
-	URL_SANDBOX    string = "http://gw.api.tbsandbox.com/router/rest" // 沙箱环境 (或者: http://gw.api.tbsandbox.com/router/rest)
-)
